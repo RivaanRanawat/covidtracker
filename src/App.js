05624092e978256e439f7c64f1fb904313354e5b
Rivaan Ranawat <namanrivaan@gmail.com>
2020-08-16 16:04:24 +0530

    Getting Countries in Dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
-import "./App.css";
+import React, {useState, useEffect} from 'react';
+import {FormControl, MenuItem, Select } from  "@material-ui/core";
+import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    {/* getting all the countries*/}
+    const getCountriesData = async() => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((res) => res.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }))
+        setCountries(countries);
+      })
+    }
+    getCountriesData();
+  }, [])
   return (
-    <div className="App">
+    <div>
+      <div className="app_header">
       <h1>Covid 19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="PUT">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      
     </div>
   );
 }
